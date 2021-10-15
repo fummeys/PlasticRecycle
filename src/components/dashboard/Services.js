5f828384a0909b3bf6d50e0b035e8b20f39fbec9
fummeys <53976947+fummeys@users.noreply.github.com>
2021-10-15 10:51:37 +0100

    camera moudule installed

--- src/components/dashboard/Services.js
+++ src/components/dashboard/Services.js
@@ -1,6 +1,9 @@
 import React,{ useState, useEffect} from 'react'
-import { View, Text, PermissionsAndroid, Button} from 'react-native'
+import { View, Text, PermissionsAndroid, Button, StyleSheet, TouchableOpacity } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { RNCamera } from 'react-native-camera'
+import Camera from './Camera'
+
 
 const readfile = ()=>{
     const permm = PermissionsAndroid.request(
@@ -36,6 +39,12 @@ const Services = ()=>{
       setuser(userObj)
     })
   }, [])
+  const takePicture = async function(camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
+  };
   // console.log(typeof(user))
   
   // let client = JSON.parse(user)
@@ -181,8 +190,57 @@ const Services = ()=>{
           <Text>Request coins</Text>
           <Button title="Request Coins" onPress={requestCoins}/>
         </View>
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        >
+          {({ camera, status, recordAudioPermissionStatus }) => {
+            if (status !== 'READY') return <Camera />;
+            return (
+              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={() => takePicture(camera)} style={styles.capture}>
+                  <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        </RNCamera>
       </>
     )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 export default Services;
